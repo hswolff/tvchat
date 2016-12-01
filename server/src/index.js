7f2fb8771534817e2ee25d1457dcb11bed672c46
Harry Wolff <hswolff@gmail.com>
2016-11-30 21:52:50 -0500

    server(config): Properly set default values on env value

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,14 @@
 require('reify');
 const _ = require('lodash');
 
-const config = require('./config').default;
-_.defaults(process.env, config);
-
 // Setup ENV values.
 try {
   const config = require('./config.local').default;
   _.defaults(process.env, config);
 } catch (e) {}
+
+const config = require('./config').default;
+_.defaults(process.env, config);
 
 // Make sure it's a number.
 process.env.PORT = (+process.env.PORT);
